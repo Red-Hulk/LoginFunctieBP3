d94079ba5c2eff58e162cdc6a7d087b34ad01af5
Red-Hulk <78483207+Red-Hulk@users.noreply.github.com>
2021-02-18 19:00:20 +0100

    Base project login

---
+++ Code1/src/sample/GUIClasses/GUILogin.java
@@ -0,0 +1,24 @@
+package sample.GUIClasses;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GUILogin {
+
+    public GUILogin(Stage primaryStage) throws IOException {
+
+        primaryStage.setMinWidth(700);
+        primaryStage.setMinHeight(500);
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+        primaryStage.setTitle("Login Scherm");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+}
---
+++ Code1/src/sample/Main.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import sample.GUIClasses.GUILogin;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        GUILogin voorbeeld = new GUILogin(primaryStage);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
